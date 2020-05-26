8523ba775edf3ca5e0bc68392f366e9de2ef25f2
John Penny <jpenny1993@users.noreply.github.com>
2020-05-26 12:45:03 +0100

    Added console test code

---
+++ examples/JPenny.TaskExtensions.ConsoleApp/DummyTasks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Audacia.Random.Extensions;
+
+namespace JPenny.TaskExtensions.ConsoleApp
+{
+    internal class DummyTasks
+    {
+        private Random Random { get; } = new Random(989852);
+
+        public Task<T> GetCancelledTask<T>()
+        {
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+            return Task.FromCanceled<T>(cts.Token);
+        }
+
+        public Task<int> GetNumber()
+        {
+            var number = Random.Next(1, 1000);
+            return Task.FromResult(number);
+        }
+
+        public Task<string> GetString()
+        {
+            var str = Random.Sentence();
+            return Task.FromResult(str);
+        }
+
+        public Task<TResult> ThrowException<TResult, TException>(TException exception)
+            where TException : Exception
+        {
+            return Task.FromException<TResult>(exception);
+        }
+
+        public Task<TResult> ThrowException<TResult, TException>()
+            where TException : Exception, new()
+        {
+            return ThrowException<TResult, Exception>(new TException());
+        }
+
+        public Task<TResult> ThrowSystemException<TResult>()
+        {
+            return ThrowException<TResult, Exception>(new Exception("Kaboom!"));
+        }
+    }
+}
--- examples/JPenny.TaskExtensions.ConsoleApp/Program.cs
+++ examples/JPenny.TaskExtensions.ConsoleApp/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace JPenny.TaskExtensions.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tasks = new DummyTasks();
+
+            await tasks.GetNumber()
+                .OnException(ex => Console.WriteLine($"Error: {ex.Flatten().GetType().Name} | {ex.Flatten().Message}"))
+                .OnCancelled(() => Console.WriteLine("Cancelled stage 1"))
+                .OnSuccess(r => Console.WriteLine($"Success: number is {r}"))
+                .Finally(x => Console.WriteLine("Finished stage 1"))
+            .Then(n => tasks.GetString())
+                .OnException(ex => Console.WriteLine($"Error: {ex.Flatten().GetType().Name} | {ex.Flatten().Message}"))
+                .OnCancelled(() => Console.WriteLine("Cancelled stage 2"))
+                .OnSuccess(r => Console.WriteLine($"Success: string is {r}"))
+                .Finally(x => Console.WriteLine("Finished stage 2"))
+            .Then(n => tasks.ThrowException<int, NotImplementedException>())
+                .OnException(ex => Console.WriteLine($"Error: {ex.Flatten().GetType().Name} | {ex.Flatten().Message}"))
+                .OnException((NotImplementedException ex) => Console.WriteLine($"Caught: {ex.GetType().Name} | {ex.Message}"))
+                .OnCancelled(() => Console.WriteLine("Cancelled stage 3"))
+                .OnSuccess(r => Console.WriteLine($"Success: this should never happen"))
+                .Finally(x => Console.WriteLine("Finished stage 3"));
+
+            Console.ReadLine();
         }
     }
 }
