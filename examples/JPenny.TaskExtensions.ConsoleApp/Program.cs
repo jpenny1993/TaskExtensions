52338a1dd7eefbd86b4125684a1ddf82963aa448
John Penny <jpenny1993@users.noreply.github.com>
2020-05-26 10:08:20 +0100

    Added projects

---
+++ examples/JPenny.TaskExtensions.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JPenny.TaskExtensions.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/JPenny.TaskExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace JPenny.TaskExtensions.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
