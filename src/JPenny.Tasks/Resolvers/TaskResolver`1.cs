d72a14ff7e1ab2d783067ebd549a370ea057eacb
John Penny <jpenny1993@users.noreply.github.com>
2021-08-29 11:42:04 +0100

    Extract task resolvers into separate classes

---
+++ src/JPenny.Tasks/Resolvers/TaskResolver`1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JPenny.Tasks.Resolvers
+{
+    public class TaskResolver<TPreviousResult> : ITaskResolver
+    {
+        private IPipelineTask<TPreviousResult> _previousTask;
+        private Func<TPreviousResult, Task> _resolveFunc;
+
+        public TaskResolver(
+            IPipelineTask<TPreviousResult> previousTask,
+            Func<TPreviousResult, Task> resolveFunc)
+        {
+            _previousTask = previousTask;
+            _resolveFunc = resolveFunc;
+        }
+
+        public TaskResolver(
+            IPipelineTask<TPreviousResult> previousTask,
+            Action<TPreviousResult> action)
+            : this(previousTask, (result) => new Task(() => action(result)))
+        {
+        }
+
+        public Task Resolve()
+        {
+            return _resolveFunc(_previousTask.Result);
+        }
+    }
+}
---
+++ src/JPenny.Tasks/Resolvers/TaskResolver`2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JPenny.Tasks.Resolvers
+{
+    public class TaskResolver<TPreviousResult, TResult> : ITaskResolver
+    {
+        private IPipelineTask<TPreviousResult> _previousTask;
+        Func<TPreviousResult, Task<TResult>> _resolveFunc;
+
+        public TaskResolver(
+            IPipelineTask<TPreviousResult> previousTask,
+            Func<TPreviousResult, Task<TResult>> resolveFunc)
+        {
+            _previousTask = previousTask;
+            _resolveFunc = resolveFunc;
+        }
+
+        public TaskResolver(
+            IPipelineTask<TPreviousResult> previousTask,
+            Func<TPreviousResult, TResult> resolveFunc)
+            :this(previousTask, (result) => Task.FromResult(resolveFunc(result)))
+        {
+        }
+
+        public Task Resolve()
+        {
+            return _resolveFunc(_previousTask.Result);
+        }
+    }
+}
